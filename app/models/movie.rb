fc84f0686752ddde19b6c93f02a7673d73032b9e
Nate Wong <nathaniel.h.wong@gmail.com>
2016-11-14 22:15:05 -0600

    Completed clean up using helper methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,4 +9,8 @@ class Movie < ActiveRecord::Base
   validates :year, :numericality => { :only_integer => true,
     :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2764800}
 
+  belongs_to :director, :class_name => "Director", :foreign_key => "director_id"
+  has_many :characters, :class_name => "Character", :foreign_key => "movie_id"
+
+
 end
